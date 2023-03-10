901798517cbd0e2b222fe4f3c2e3db22528841bb
Shalin Kulawardane <nirvan.work@gmail.com>
2023-03-11 01:29:36 +0530

    Modified Saviour to allow it to decrypt files in nested directories

--- Saviour.py
+++ Saviour.py
@@ -10,15 +10,20 @@ def get_key():
 class Saviour:
     targets = []
 
-    def gather_targets(self):
-        for file in os.listdir():
-            if file == "Reaper.py" or file == "key.key" or file == "Saviour.py":
+    def gather_targets(self, path=os.getcwd()):
+        for file in os.scandir(path):
+            name = file.path.split("\\")[-1]
+
+            if name == "Reaper.py" or name == "key.key" or name == "Saviour.py":
                 continue
 
-            if os.path.isdir(file):
-                continue
+            if file.is_dir():
+                if name == "venv" or name == ".git" or name == "inspectionProfiles" or name == ".idea":
+                    continue
+                self.gather_targets(file)
 
-            self.targets.append(file)
+            else:
+                self.targets.append(file.path)
 
     def decrypt(self, key_to_decrypt):
         for file in self.targets:
