aee27917c4f6118315e3e11f7c86a7d8175c1f90
Shalin Kulawardane <nirvan.work@gmail.com>
2023-03-15 01:05:57 +0530

    Made minor changes

--- Papa.py
+++ Papa.py
@@ -1,19 +1,21 @@
 import socket
 
 SERVER_IP = "192.168.1.176"
-PORT = "4678"
+PORT = 4678
 
 # Create the socket using "with" to automatically close the socket
 with (socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+    print("Listening...")
     # Bind the IP address to the port
-    sock.bind(SERVER_IP, PORT)
+    sock.bind((SERVER_IP, PORT))
     sock.listen(1)
     
-    # When a connection is accepted, an instance representing 
+    # When a connection is accepted, an instance representing
     # the client and server's connection and the address of the client
     connection, client_ip = sock.accept()
 
-    with connection:
+    with (connection):
+        print(f"Connection was established with: {client_ip}")
         while True:
             # Data can only be sent as binary. Prefix it with 'b'
             connection.send(b"Hehehe...")
--- Reaper_client.py
+++ Reaper_client.py
@@ -2,13 +2,13 @@ import socket
 from Reaper import Reaper
 
 SERVER_IP = "192.168.1.176"
-SERVER_PORT = "4678"
+SERVER_PORT = 4678
 
 class Reaper_client(Reaper):
 
     def connect(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect(SERVER_IP, SERVER_PORT)
+            sock.connect((SERVER_IP, SERVER_PORT))
             data = sock.recv(1024)
             print(f"Received: {data}")
 
